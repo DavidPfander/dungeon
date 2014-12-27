b3edb5f8b7913cde0f0016d6c1d7ae91efdf5cab
Jannis <JannisBeese@gmail.com>
2014-12-27 19:02:10 +0100

    Merge commit

--- Dungeon/src/maps.lua
+++ Dungeon/src/maps.lua
@@ -1,16 +1,34 @@
 local P = {}
 maps = P
 
-maps.fillFactor = .7 -- Percentage of floor tiles
+maps.fillFactor = .5 -- Percentage of floor tiles
+maps.currentFactor = 0
+maps.numFloorTiles = 0
 
 function maps.generateMap(sizeX, sizeY)
+
+  if sizeX < 5 or sizeY < 5 then
+    print("Error: Map size too small.")
+    return 1
+  end
+
   map = {}
   math.randomseed(os.time())
+  maps.mapSize = sizeX * sizeY
   for curX = 0,sizeX,1 do
   map[curX] = {}
     for curY = 0,sizeY,1 do 
       map[curX][curY] = 1 -- default: Fill with walls
     end  
   end
+  
+  -- Now generate rooms as long as the map has not enough floor tiles
+  while (currentFactor < fillFactor) do
+    roomLowerX = math.rand(1, sizeX - 2)
+    roomLowerY = math.rand(1, sizeY - 2)
+    
+    
+  end
+  
   return map
 end
