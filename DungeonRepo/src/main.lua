3dde8f1816b9528b86539b9179bc9ad64d016ba8
pfandedd <pfandedd@ipvs.uni-stuttgart.de>
2014-12-27 18:15:03 +0100

    - more modularization

--- DungeonRepo/src/main.lua
+++ DungeonRepo/src/main.lua
@@ -1,68 +1,79 @@
+require "figure"
+require "adversary"
+
 function love.load()
-    player = {
-        grid_x = 256,
-        grid_y = 256,
-        act_x = 200,
-        act_y = 200,
-        speed = 10
-    }
-    map = {
-        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-        { 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
-        { 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1 },
-        { 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1 },
-        { 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1 },
-        { 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
-        { 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-        { 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-        { 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-        { 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1 },
-        { 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
-        { 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
-        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
-    }
+  --    player = {
+  --        grid_x = 256,
+  --        grid_y = 256,
+  --        act_x = 200,
+  --        act_y = 200,
+  --        speed = 10
+  --    }
+
+  map = {
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
+    { 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1 },
+    { 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1 },
+    { 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1 },
+    { 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+  }
+
+  -- place player
+  player = figure.new()
+
+  -- place enemy
+  enemyCount = 10
+  gridSizeX = 13
+  gridSizeY = 13
+  enemies = adversary.placeEnemies(map, enemyCount, gridSizeX, gridSizeY)
 end
 
 
 function love.update(dt)
-    player.act_y = player.act_y - ((player.act_y - player.grid_y) * player.speed * dt)
-    player.act_x = player.act_x - ((player.act_x - player.grid_x) * player.speed * dt)
+  figure.update(dt, player)
 end
 
 function love.draw()
-    love.graphics.rectangle("fill", player.act_x, player.act_y, 32, 32)
-    for y=1, #map do
-        for x=1, #map[y] do
-            if map[y][x] == 1 then
-                love.graphics.rectangle("line", x * 32, y * 32, 32, 32)
-            end
-        end
+  -- drawing all cells at same priority right now
+
+  love.graphics.setColor(255, 255, 255)
+  for y=1, #map do
+    for x=1, #map[y] do
+      if map[y][x] == 1 then
+        love.graphics.rectangle("line", x * 32, y * 32, 32, 32)
+      end
     end
+  end
+  
+  for i = 1, #enemies do
+    adversary.draw(enemies[i])
+  end
+  
+  figure.draw(player)
 end
 
 function love.keypressed(key)
-    if key == "up" then
-        if testMap(0, -1) then
-            player.grid_y = player.grid_y - 32
-        end
-    elseif key == "down" then
-        if testMap(0, 1) then
-            player.grid_y = player.grid_y + 32
-        end
-    elseif key == "left" then
-        if testMap(-1, 0) then
-            player.grid_x = player.grid_x - 32
-        end
-    elseif key == "right" then
-        if testMap(1, 0) then
-            player.grid_x = player.grid_x + 32
-        end
-    end
+  figure.keypressed(player, key, map)
 end
 
-function testMap(x, y)
-    if map[(player.grid_y / 32) + y][(player.grid_x / 32) + x] == 1 then
-        return false
-    end
-    return true
+function testMap(map, x, y)
+--  print("printing map")
+--  for i = 0, #map do
+--    for j = 0, #map[i] do
+--      print(map[i][j])
+--    end
+--  end
+--  print (map[y])
+  if map[y][x] == 1 then
+    return false
+  end
+  return true
 end
