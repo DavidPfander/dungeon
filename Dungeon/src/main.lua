1c950e1e6af2c51b2bb2d149cbbfd46a91b3f02b
pfandedd <pfandedd@ipvs.uni-stuttgart.de>
2014-12-27 20:41:43 +0100

    - merged

--- Dungeon/src/main.lua
+++ Dungeon/src/main.lua
@@ -1,5 +1,6 @@
 require "figure"
 require "adversary"
+require "maps"
 
 function love.load()
   --    player = {
@@ -9,6 +10,12 @@ function love.load()
   --        act_y = 200,
   --        speed = 10
   --    }
+
+
+  -- place enemy
+  enemyCount = 10
+  gridSizeX = 16
+  gridSizeY = 16
 
   map = {
     { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
@@ -25,25 +32,40 @@ function love.load()
     { 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
     { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
   }
+  
+  map = maps.generateMap(gridSizeX,gridSizeY)
 
-  -- place player
-  player = figure.new()
-
-  -- place enemy
-  enemyCount = 10
-  gridSizeX = 13
-  gridSizeY = 13
+  gameWon = false
   enemies = adversary.placeEnemies(map, enemyCount, gridSizeX, gridSizeY)
 end
 
+function removeEnemy(gridX, gridY)
+  for i = 1, #enemies do
+    local enemy = enemies[i]
+    print("mineX: " .. gridX .. " enemyX: " .. enemy.gridX)
+    print("mineY: " .. gridY .. " enemyY: " .. enemy.gridY)
+    if enemy.gridX == gridX and enemy.gridY == gridY then
+      table.remove(enemies, i)
+      print("removing enemy")
+      enemyCount = enemyCount - 1
+      break
+    end
+  end
+end
 
 function love.update(dt)
-  figure.update(dt, player)
+  if enemyCount == 0 then
+    gameWon = true
+  else
+    figure.update(dt, player)
+    for i = 1, #enemies do
+      adversary.update(dt, enemies[i])
+    end
+  end
 end
 
 function love.draw()
   -- drawing all cells at same priority right now
-
   love.graphics.setColor(255, 255, 255)
   for y=1, #map do
     for x=1, #map[y] do
@@ -56,11 +78,19 @@ function love.draw()
   for i = 1, #enemies do
     adversary.draw(enemies[i])
   end
-  
+
   figure.draw(player)
+
+  if gameWon then
+    love.graphics.setFont(love.graphics.newFont(40))
+    love.graphics.print('You win!!', 400, 300)
+  end
 end
 
 function love.keypressed(key)
+  if gameWon then
+    return
+  end
   figure.keypressed(player, key, map)
 end
 
@@ -80,4 +110,12 @@ end
 
 function registerEnemyMap(map, x, y)
   map[y][x] = 2
+end
+
+function enemyHitMap(map, x, y)
+  if map[y][x] == 2 then
+    return true
+  else
+    return false
+  end
 end
