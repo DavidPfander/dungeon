474fd7998ae28e2469364662ea336e0fc7009a18
pfandedd <pfandedd@ipvs.uni-stuttgart.de>
2014-12-28 22:05:58 +0100

    - less console output

--- Dungeon/src/items.lua
+++ Dungeon/src/items.lua
@@ -42,9 +42,7 @@ function items.placeItems(map)
     local gridX = math.random(2, gridSizeX - 1)
     local gridY = math.random(2, gridSizeY - 1)
     if maps.testMove(map, gridX, gridY) then
-      print("placing item at " .. gridX .. ", " .. gridY .. " level:" .. level)
       map[gridX][gridY].items[#map[gridX][gridY].items + 1] = item
-      print(#map[gridX][gridY].items)
       itemsPlaced = itemsPlaced + 1
     end
   end
@@ -53,30 +51,19 @@ end
 function items.draw(map)
   for x = 1, #map do
     for y = 1, #map[x] do
-      if #map[x][y].items > 0 then
-        print("item found2")
-      end
-    end
-  end
+      if math.abs(player.gridX - x) > vision or math.abs(player.gridY - y) > vision then
+        -- skip
+      else
+        for i = 1, #map[x][y].items do
+          local item = map[x][y].items[i]
+          local lighting = util.getEnemyLighting(x, y)
+          --local lighting = 255
+          love.graphics.setColor(lighting, lighting, lighting)
+          local image = love.graphics.newImage(item.image)
+          love.graphics.draw(image, x * util.pixelPerCellX, y * util.pixelPerCellY)
+        end
 
-  for x = 1, #map do
-    for y = 1, #map[x] do
-      if #map[x][y].items > 0 then
-        print(#map[x][y].items)
       end
-      --      if math.abs(player.gridX - x) > vision or math.abs(player.gridY - y) > vision then
-      --      -- skip
-      --      else
-      for i = 1, #map[x][y].items do
-        local item = map[x][y].items[i]
-        local lighting = util.getEnemyLighting(x, y)
-        --local lighting = 255
-        love.graphics.setColor(lighting, lighting, lighting)
-        local image = love.graphics.newImage(item.image)
-        love.graphics.draw(image, x * util.pixelPerCellX, y * util.pixelPerCellY)
-      end
-     
-      -- end
     end
   end
 end
